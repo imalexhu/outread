30b870940dbde72f73a4c291874fb907bcaa7880
Alex Hu <imalexhu@gmail.com>
2024-07-08 14:18:35 +0930

    Add : Created a caching system to cache preprocessed data

--- DataProcessing.py
+++ DataProcessing.py
@@ -9,32 +9,58 @@ from nltk.stem import WordNetLemmatizer
 
 
 class DataProcessing :
-    def __init__(self,folder_path, config) -> None:
+    def __init__(self, config,folder_path) -> None:
         nltk.download('stopwords')
         nltk.download('wordnet')
         self.folder_path = folder_path
         self.config = config
     
+    def get_cache_path(self,pdf_file_name : str) -> str:
+        file_path = ""
+        
+        if self.config.get('DataProcessing') == 'Stemming':
+            file_path = os.path.join('cache_s', pdf_file_name)
+        else :
+            file_path = os.path.join('cache_l', pdf_file_name)
+        return file_path
+    
+    def load_from_cache(self,pdf_file_name : str) -> str:
+        with open(self.get_cache_path(pdf_file_name), 'r') as file:
+            text = file.read()
+            return text
+        
     def extract_text_from_pdfs(self):
         pdf_files = [f for f in os.listdir(self.folder_path) if f.endswith('.pdf')]
         all_texts = {}
-
-        for pdf_file in pdf_files:
-            pdf_path = os.path.join(self.folder_path, pdf_file)
-            with open(pdf_path, 'rb') as file:
-                reader = PdfReader(file)
-                text = "Hello I am Alexander, and I am 8 years old. I feel good. I am happy"
-                # for page in reader.pages:
-                #     text += page.extract_text()
-                # print(pdf_file)
-                all_texts[pdf_file] = text
+        for pdf_file_name in pdf_files:
+            # check if the file is in cache
+            pdf_content = ""
+            if os.path.exists(self.get_cache_path(pdf_file_name)):
+                pdf_content = self.load_from_cache(pdf_file_name)
+            else : 
+                print(f"Extracting text from {pdf_file_name}")
+                pdf_path = os.path.join(self.folder_path, pdf_file_name)
+                with open(pdf_path, 'rb') as file:
+                    reader = PdfReader(file)
+                    for page in reader.pages:
+                        pdf_content += page.extract_text()
+                        
+                # store in cache
+                if(self.config.get('DataProcessing') == 'Stemming'):
+                    os.makedirs("cache_s", exist_ok=True)
+                else :
+                    os.makedirs("cache_l", exist_ok=True)
+                with open(self.get_cache_path(pdf_file_name), 'w') as cache_file:
+                    cache_file.write(pdf_content)
+                all_texts[pdf_file_name] = pdf_content
         return all_texts
 
-    def preprocess(self,papers : dict[str,str]) :
+    def preprocess(self,papers : dict[str,str]) -> dict[str,str]:
         # Preprocess the text
         for name in papers :   
             text  = papers[name]
             text = text.lower()
+            
             # Remove punctuation
             text = re.sub(r'[^a-z\s]', '', text)
 
@@ -55,7 +81,7 @@ class DataProcessing :
         return papers
 
     def process_texts(self) -> str:
-        all_texts = self.extract_text_from_pdfs(self.folder_path)
+        all_texts = self.extract_text_from_pdfs()
         return self.preprocess(all_texts)
 
 
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 
 from DataProcessing import DataProcessing
+# from Vectorise import Vectorise
+# from Output import Output
+# from Visualise import Visualise
 
 config = {
-    "DataProcessing" : "Stemming" 
-    # "DataProcessing" : "Lemmatization
+    # "DataProcessing" : "Stemming" ,
+    "DataProcessing" : "Lemmatization",
+    "Vectorise" : "TF-IDF",
+    # "Vectorise": "Word2Vec",
 }
 
 
 
 def main():
     # preprocess the Gree Energy Datasets
-    dp = DataProcessing(config, 'Green Energy Dataset').process_texts()
+    processed_results = DataProcessing(config, 'Green Energy Dataset').process_texts()
+    # vectoried_results = Vectorise(config,processed_results).vectorise_data()
+    # clustered_results = Cluster(vectoried_results,config).cluster_data()
+    # Output(clustered_results).genereate()
+    # Visualise(clustered_result).generate()
 
 
 if __name__ == "__main__":
